6dd2b5c555660ca1b31a6ed776265dbf5499dd93
vedantkasargod <113184175+vedantkasargod@users.noreply.github.com>
2024-09-17 14:38:03 +0530

    Update activations.py

--- activations.py
+++ activations.py
@@ -101,7 +101,6 @@ class Maxout(CustomActivationFunctionBase):
         return torch.max(x, dim=1).values
 
 
-# 
     
 class CombinedActivation(nn.Module):
     def __init__(self, activations):
@@ -115,19 +114,3 @@ class CombinedActivation(nn.Module):
 
 
 
-activation_fn = ExponentialFunction(alpha=0.001)
-x = torch.Tensor([-2, -1, 0, 1, 2])
-output = activation_fn(x)
-print(output)
-
-combined_activation_fn = CombinedActivation([LeakyReLU(),
-        ELU(alpha=1.0),
-        GELU(),
-        PReLU(init=0.25),
-        Maxout(input_dim=5, num_units=2),  # Adjust input_dim and num_units based on model
-        PiecewiseLinearActivation(a1=1, b1=0, a2=0.5, b2=1, c=0),
-        ExponentialFunction(alpha=0.5),
-        Softplus()])
-y = torch.Tensor([-2, -1, 0, 1, 2])
-output_combined = combined_activation_fn(y)
-print(output_combined)
